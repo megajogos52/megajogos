635a563713a3ddc161dfc16b8308bec517a9f383
Wermesson <wermesson.pessoal@gmail.com>
2025-08-05 12:24:22 -0300

    Atualização do site

--- javaScript/inicio.js
+++ javaScript/inicio.js
@@ -1,23 +1,45 @@
 function pesquisar() {
-    var valorPesquisa = document.getElementById('termo').value.toLowerCase();
-    valorPesquisa = valorPesquisa.replace(/ /g, "-");
-    console.log(valorPesquisa);
+  var valorPesquisa = document.getElementById('termo').value.toLowerCase();
+  valorPesquisa = valorPesquisa.replace(/ /g, "-");
+  console.log(valorPesquisa);
 
-    const jogos = document.querySelectorAll('.jogo');
-  
-    jogos.forEach(jogo => {
-      // Remove todos primeiro
-      jogo.style.display = 'none';
-  
-      // Se a div contém a classe pesquisada, mostra
-      if (jogo.classList.contains(valorPesquisa)) {
-        jogo.style.display = 'block';
-      }
+  const jogos = document.querySelectorAll('.jogo');
 
-      if(valorPesquisa === ""){
-        jogo.style.display = 'block';
-      }
-      
-    });
-    
+  jogos.forEach(jogo => {
+    // Remove todos primeiro
+    jogo.style.display = 'none';
+
+    // Se a div contém a classe pesquisada, mostra
+    if (jogo.classList.contains(valorPesquisa)) {
+      jogo.style.display = 'block';
+    }
+
+    if (valorPesquisa === "") {
+      jogo.style.display = 'block';
+    }
+
+  });
+
+}
+
+function mostrar(nome) {
+  esconderTodos();
+  mudarTitulo(nome);
+  var elementos = document.getElementsByClassName(nome);
+  for (var i = 0; i < elementos.length; i++) {
+    elementos[i].style.display = 'block';
   }
+}
+
+function esconderTodos() {
+ var elementos = document.querySelectorAll('.apps, .jogos');
+  for (var i = 0; i < elementos.length; i++) {
+    elementos[i].style.display = 'none';
+  }
+}
+
+function mudarTitulo(nome){
+  var titulo = document.getElementById('titulo');
+
+  titulo.textContent = (nome + ' disponíveis').toUpperCase();
+}
